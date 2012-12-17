45f58a18ef8d6a457cab4eee1a18347b59a7ac57
Federico Gonzalez <fedegl@gmail.com>
2012-12-18 10:52:47 +1300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HarvesterManager::Application.config.secret_token = '2f00f435132c5527e8d83dae24885a2d610cc60a5fb55658e22a6f9b64adb8e7f6b13519a42cc5a59f9eee38949ee7570958002b083a2ef7c01f1c1c075b39c8'
