f0dd5296f6a33d4e167651fed44d62b21725c065
Andy Gray <andy.gray@boost.co.nz>
2013-09-17 10:49:00 +1200

    STORY [56324064] AUTOMATIC SOURCE ID: As a harvest operator, I don't want to have to specify a source_id in my parsers so that mistakes are minimised.

--- app/serializers/parser_serializer.rb
+++ app/serializers/parser_serializer.rb
@@ -1,5 +1,5 @@
 class ParserSerializer < ActiveModel::Serializer
   
-  attributes :id, :name, :strategy, :content, :file_name
+  attributes :id, :name, :strategy, :content, :file_name, :source
 
 end
