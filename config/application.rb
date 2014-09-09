563de7d493499e0255dc2d0e6f754195adc878d7
Dave Harris <dave@boost.co.nz>
2014-09-09 15:16:59 +1200

    Skip i18n locale validation

--- config/application.rb
+++ config/application.rb
@@ -88,5 +88,7 @@ module HarvesterManager
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.i18n.enforce_available_locales = false
   end
 end
