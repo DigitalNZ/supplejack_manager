aa5b2225c49ec8b49c8b3fa9d482b2e4bb3947bf
buyifly <buyifly@live.com>
2016-07-11 09:47:03 +1200

    - Displaying version of parser on parser show. - Layout change for parser controls.

--- Gemfile
+++ Gemfile
@@ -34,7 +34,10 @@ gem 'activeresource', require: 'active_resource'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 gem 'lograge'
-gem 'airbrake', '~> 5.2'
+
+group :staging, :production do
+  gem 'airbrake', '~> 5.2'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -1,9 +1,9 @@
 # The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are 
-# third party components that are licensed under the MIT license or otherwise publicly available. 
-# See https://github.com/DigitalNZ/supplejack_manager for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs. 
+# and is licensed under the GNU General Public License, version 3. Some components are
+# third party components that are licensed under the MIT license or otherwise publicly available.
+# See https://github.com/DigitalNZ/supplejack_manager for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
 # http://digitalnz.org/supplejack
 
 class ParsersController < ApplicationController
@@ -71,7 +71,7 @@ class ParsersController < ApplicationController
 
     if @parser.save
       if params[:allow] == 'false'
-        HarvestSchedule.update_schedulers_from_environment({parser_id: @parser.id}, "staging") 
+        HarvestSchedule.update_schedulers_from_environment({parser_id: @parser.id}, "staging")
         HarvestSchedule.update_schedulers_from_environment({parser_id: @parser.id}, "production")
       end
       respond_to do |format|
@@ -79,7 +79,7 @@ class ParsersController < ApplicationController
         format.js
       end
     else
-      redirect_to edit_parser_path(@parser)  
+      redirect_to edit_parser_path(@parser)
     end
   end
 end
