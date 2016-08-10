f074c800e00dea37cf9c7aaf721289bcc171cc0b
yarhtut <yar@boost.co.nz>
2016-08-10 14:54:09 +1200

    add comments for module Overwirte class file

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
 module Rack
+  # Overwriting Rack file for deprecations
   class File
+    # Returns error/deprecations to console on test.
+    #
+    # @author Yar
+    # @last_modified Yar
+    # @param * []
+    # @return [String] errors
     def warn(*)
     end
   end
 end
-
