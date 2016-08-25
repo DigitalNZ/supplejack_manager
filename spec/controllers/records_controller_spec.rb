2101894a52bbf9eb033a9350d9db0a439644342d
Edwin Rozario <edwin@boost.co.nz>
2016-08-25 17:19:46 +1200

    Commented all specs in RecordsController as the controller is being retired

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -13,50 +13,50 @@ require 'spec_helper'
 
 describe RecordsController do
 
-  before(:each) do
-    controller.stub(:authenticate_user!) { true }
-    controller.stub(:current_user) { mock(:user, id: 123) }
-  end
+  # before(:each) do
+  #   controller.stub(:authenticate_user!) { true }
+  #   controller.stub(:current_user) { mock(:user, id: 123) }
+  # end
   
-  let(:parser) { mock(:parser).as_null_object }
-  let(:previewer) { mock(:previewer).as_null_object }
-  let(:harvester) { mock(:harvester).as_null_object }
+  # let(:parser) { mock(:parser).as_null_object }
+  # let(:previewer) { mock(:previewer).as_null_object }
+  # let(:harvester) { mock(:harvester).as_null_object }
 
-  describe 'POST index' do
-    before do
-      Parser.stub(:find) { parser }
-      Previewer.stub(:new) { previewer } 
-    end
+  # describe 'POST index' do
+  #   before do
+  #     Parser.stub(:find) { parser }
+  #     Previewer.stub(:new) { previewer } 
+  #   end
 
-    it 'should call before filters for find_parser_and_version' do
-      controller.should_receive(:find_parser_and_version)
-      post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
-    end
+  #   it 'should call before filters for find_parser_and_version' do
+  #     controller.should_receive(:find_parser_and_version)
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
+  #   end
 
-    it 'should call before filters for set_previewer' do
-      controller.should_receive(:set_previewer)
-      post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
-    end
+  #   it 'should call before filters for set_previewer' do
+  #     controller.should_receive(:set_previewer)
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
+  #   end
     
-    it 'finds the parser' do
-      Parser.should_receive(:find).with('1234') { parser }
-      post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
-    end
+  #   it 'finds the parser' do
+  #     Parser.should_receive(:find).with('1234') { parser }
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {}, format: :js
+  #   end
 
-    it 'initializes a previewer object' do
-      Previewer.should_receive(:new).with(parser, 'Data', 123, 10, nil) { previewer }
-      post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, format: :js
-      assigns(:previewer).should eq previewer
-    end
+  #   it 'initializes a previewer object' do
+  #     Previewer.should_receive(:new).with(parser, 'Data', 123, 10, nil) { previewer }
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, format: :js
+  #     assigns(:previewer).should eq previewer
+  #   end
 
-    it 'initializes a new previewer in test mode' do
-      Previewer.should_receive(:new).with(parser, 'Data', 123, 10, nil) { previewer }
-      post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, environment: 'test', format: :js
-    end
+  #   it 'initializes a new previewer in test mode' do
+  #     Previewer.should_receive(:new).with(parser, 'Data', 123, 10, nil) { previewer }
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, environment: 'test', format: :js
+  #   end
 
-    it 'should preview the records from a existing harvest' do
-      Previewer.should_receive(:new).with(parser, 'Data', 123, 10, true) { previewer }
-      post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, environment: 'test', review: true, format: :js
-    end
-  end
+  #   it 'should preview the records from a existing harvest' do
+  #     Previewer.should_receive(:new).with(parser, 'Data', 123, 10, true) { previewer }
+  #     post :index, parser_id: '1234', version_id: '5678', parser: {content: 'Data'}, index: 10, environment: 'test', review: true, format: :js
+  #   end
+  # end
 end
