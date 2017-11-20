3cbd5442d283b513a389edba0f4cd35b7c9d21f2
Reinkaos <richard@boost.co.nz>
2017-11-20 15:30:44 +1300

    Fix the collection records controller

--- spec/controllers/collection_records_controller_spec.rb
+++ spec/controllers/collection_records_controller_spec.rb
@@ -1,15 +1,15 @@
 # The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are 
-# third party components that are licensed under the MIT license or otherwise publicly available. 
-# See https://github.com/DigitalNZ/supplejack_manager for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs. 
+# and is licensed under the GNU General Public License, version 3. Some components are
+# third party components that are licensed under the MIT license or otherwise publicly available.
+# See https://github.com/DigitalNZ/supplejack_manager for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
 # http://digitalnz.org/supplejack
 
 require 'spec_helper'
 
 describe CollectionRecordsController do
-  let(:user) { mock_model(User, role: "admin").as_null_object }
+  let(:user) { instance_double(User, role: 'admin').as_null_object }
 
   before(:each) do
     controller.stub(:authenticate_user!) { true }
@@ -17,8 +17,9 @@ describe CollectionRecordsController do
   end
 
   describe 'GET #index' do
-    it "should be successful" do
-      get :index, environment: "development"
+    it 'should be successful' do
+      RestClient.should_receive(:get).with("#{ENV['API_HOST']}/harvester/records/.json", params: { api_key: ENV['HARVESTER_API_KEY'] })
+      get :index, environment: 'development'
       response.should be_success
     end
 
