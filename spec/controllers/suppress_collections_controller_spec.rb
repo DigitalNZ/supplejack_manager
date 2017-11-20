aaebb18347ee8a4ba6a5390a1b643266c88fd35a
Reinkaos <richard@boost.co.nz>
2017-11-20 15:33:58 +1300

    Fix the suppress collections controller spec

--- spec/controllers/suppress_collections_controller_spec.rb
+++ spec/controllers/suppress_collections_controller_spec.rb
@@ -1,15 +1,15 @@
 # The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are 
-# third party components that are licensed under the MIT license or otherwise publicly available. 
-# See https://github.com/DigitalNZ/supplejack_manager for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs. 
+# and is licensed under the GNU General Public License, version 3. Some components are
+# third party components that are licensed under the MIT license or otherwise publicly available.
+# See https://github.com/DigitalNZ/supplejack_manager for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
 # http://digitalnz.org/supplejack
 
 require "spec_helper"
 
 describe SuppressCollectionsController do
-  let(:user) { mock_model(User, role: 'admin').as_null_object }
+  let(:user) { instance_double(User, role: 'admin').as_null_object }
 
   before(:each) do
     controller.stub(:authenticate_user!) { true }
