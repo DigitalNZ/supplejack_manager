0bc4f4705c160c1a113cc3d396cf96ec8f881f6c
Reinkaos <richard@boost.co.nz>
2018-01-09 12:06:01 +1300

    Remove double from snippets controller spec

--- spec/factories/snippet.rb
+++ spec/factories/snippet.rb
@@ -8,7 +8,7 @@
 
 FactoryBot.define do
   factory :snippet do
-    # name      "Copyright"
-    # content   "module Copyright; end"
+    name      'Copyright'
+    content   'module Copyright; end'
   end
 end
