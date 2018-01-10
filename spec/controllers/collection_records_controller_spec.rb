09d189f85242b7b74e39c5145b36b93783189775
Reinkaos <richard@boost.co.nz>
2018-01-11 10:07:04 +1300

    fix collection records controller

--- spec/controllers/collection_records_controller_spec.rb
+++ spec/controllers/collection_records_controller_spec.rb
@@ -16,30 +16,30 @@ describe CollectionRecordsController do
   describe 'GET #index' do
     it 'should be successful' do
       expect(RestClient).to receive(:get).with("#{ENV['API_HOST']}/harvester/records/.json", params: { api_key: ENV['HARVESTER_API_KEY'] })
-      get :index, environment: 'development'
+      get :index, params: { environment: 'development' }
       expect(response).to be_success
     end
 
     it 'should search for a record' do
       expect(RestClient).to receive(:get).with("#{ENV['API_HOST']}/harvester/records/abc123.json", params: { api_key: ENV['HARVESTER_API_KEY'] })
-      get :index, environment: 'development', id: 'abc123'
+      get :index, params: { environment: 'development', id: 'abc123' }
     end
 
     it 'should handle RESTClient errors' do
       allow(RestClient).to receive(:get).and_raise(RestClient::Exception.new)
-      get :index, environment: 'development', id: 'abc123'
+      get :index, params: { environment: 'development', id: 'abc123' }
     end
   end
 
   describe 'PUT #update' do
     it 'should make a post to the api to change the status to active for the record' do
       expect(RestClient).to receive(:put).with("#{ENV['API_HOST']}/harvester/records/abc123", { record: { status: 'active' }, api_key: ENV['HARVESTER_API_KEY'] })
-      put :update, environment: 'development', id: 'abc123', status: 'active'
+      put :update, params: { environment: 'development', id: 'abc123', status: 'active' }
     end
 
     it 'should redirect to #index' do
       allow(RestClient).to receive(:put)
-      put :update, environment: 'development', id: 'abc123', status: 'active', api_key: ENV['HARVESTER_API_KEY']
+      put :update, params: { environment: 'development', id: 'abc123', status: 'active', api_key: ENV['HARVESTER_API_KEY'] }
       expect(response).to redirect_to environment_collection_records_path(environment: 'development', id: 'abc123')
     end
   end
