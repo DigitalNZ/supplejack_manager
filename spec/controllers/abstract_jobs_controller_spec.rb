820aed00cd485db643ebe485c69b74a6df6f792e
Reinkaos <richard@boost.co.nz>
2018-01-15 13:06:37 +1300

    Fix failing spec

--- spec/controllers/abstract_jobs_controller_spec.rb
+++ spec/controllers/abstract_jobs_controller_spec.rb
@@ -15,7 +15,7 @@ describe AbstractJobsController do
 
   describe 'GET index' do
     it 'returns active abstract jobs' do
-      expect(AbstractJob).to receive(:search).with(hash_including('status' => 'active'))
+      expect(AbstractJob).to receive(:search).with(status: 'active', environment: 'staging')
       get :index, params: { status: 'active', environment: 'staging' }
     end
   end
