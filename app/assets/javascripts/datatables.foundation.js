d088ba140d64b2b1d042be4ff0fc2339d18d8579
Reinkaos <richard@boost.co.nz>
2018-01-25 16:34:52 +1300

    Fix the data table pagination

--- app/assets/javascripts/datatables.foundation.js
+++ app/assets/javascripts/datatables.foundation.js
@@ -37,8 +37,8 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
             $(nPaging).addClass('pagination').append(
                 '<ul class="pagination" role="navigation" aria-label="Pagination">'+
-                    '<li class="pagination-previous disabled"><a href="#">'+oLang.sPrevious+'</a></li>'+
-                    '<li class="pagination-next"><a href="#">'+oLang.sNext+' </a></li>'+
+                    '<li class="pagination-previous disabled"><a href="#">' + oLang.sPrevious + '</a></li>' +
+                    '<li class="pagination-next"><a href="#">' + oLang.sNext + ' </a></li>' +
                 '</ul>'
             );
             var els = $('a', nPaging);
@@ -67,15 +67,21 @@ $.extend( $.fn.dataTableExt.oPagination, {
                 iEnd = iStart + iListLength - 1;
             }
 
-            for ( i=0, iLen=an.length ; i<iLen ; i++ ) {
+            for ( i=0, iLen=an.length ; i < iLen ; i++ ) {
                 // Remove the middle elements
                 $('li:gt(0)', an[i]).filter(':not(:last)').remove();
 
                 // Add the new list items and their event handlers
                 for ( j=iStart ; j<=iEnd ; j++ ) {
-                    sClass = (j==oPaging.iPage+1) ? 'class="current"' : '';
+                    sClass = (j == oPaging.iPage + 1) ? 'class="current"' : '';
 
-                    $('<li '+sClass+'><a href="#">'+j+'</a></li>')
+                    if (sClass == 'class="current"') {
+                      var markup = '<li ' + sClass + '><span class="show-for-sr"></span>' + j + '</li>';
+                    } else {
+                      var markup = '<li ' + sClass + '><a href="#">' + j + '</a></li>';
+                    }
+
+                    $(markup)
                     .insertBefore( $('li:last', an[i])[0] )
                     .bind('click', function (e) {
                         e.preventDefault();
@@ -86,15 +92,15 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
                 // Add / remove disabled classes from the static elements
                 if ( oPaging.iPage === 0 ) {
-                    $('li:first', an[i]).addClass('unavailable');
+                    $('li:first', an[i]).addClass('disabled');
                 } else {
-                    $('li:first', an[i]).removeClass('unavailable');
+                    $('li:first', an[i]).removeClass('disabled');
                 }
 
                 if ( oPaging.iPage === oPaging.iTotalPages-1 || oPaging.iTotalPages === 0 ) {
-                    $('li:last', an[i]).addClass('unavailable');
+                    $('li:last', an[i]).addClass('disabled');
                 } else {
-                    $('li:last', an[i]).removeClass('unavailable');
+                    $('li:last', an[i]).removeClass('disabled');
                 }
             }
         }
