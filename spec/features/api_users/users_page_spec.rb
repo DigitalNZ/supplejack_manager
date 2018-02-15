ba9a76ddbff9269d84975b5cd61cfad8aab0c017
Oliver Stigley <oliver@boost.co.nz>
2018-02-16 11:58:46 +1300

    signed out users visiting user admin page are redirected

--- spec/features/api_users/users_page_spec.rb
+++ spec/features/api_users/users_page_spec.rb
@@ -2,13 +2,22 @@
 require 'rails_helper'
 
 feature 'API Users Page', type: :feature do
-  before do
-    visit root_path
-  end
 
-  scenario 'can be access from the home page navigation', do
-    visit root_path
-    # click on nav item
-    expect(page.current_path).to eq environment_admin_users_path
+  context 'user signed out' do
+    scenario 'is redirected to user sign in page' do
+      visit environment_admin_users_path environment: 'test'
+      expect(page.current_path).to eq new_user_session_path
+    end
   end
+  # scenario 'can be access from the home page navigation' do
+    # before do
+    #   visit new_user_session_path
+    #    fill_in 'user[login]', with: user.username
+    #     fill_in 'user[password]', with: user.password
+    #     click_button 'Sign in'
+    # end
+  #   visit root_path
+  #   click_link('test-api-users')
+  #   expect(page.current_path).to eq environment_admin_users_path
+  # end
 end
