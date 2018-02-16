d11a26422945554dc8e3f0f28a164b7b5bbc5357
Oliver Stigley <oliver@boost.co.nz>
2018-02-16 13:35:22 +1300

    admin user page shows all user fields

--- spec/features/api_users/users_page_spec.rb
+++ spec/features/api_users/users_page_spec.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 feature 'API Users Page', type: :feature do
+  let(:admins) do
+    { id: '123123123',
+      name: 'John Doe',
+      username: 'JohnTheDoe',
+      email: 'john@email.com',
+      authentication_token: '123',
+      role: 'admin',
+      daily_requests: '5000',
+      monthly_requests: '150000',
+      max_requests: '101010101010' }.stringify_keys!
+  end
 
   context 'user signed out' do
     scenario 'is redirected to user sign in page' do
@@ -9,15 +21,36 @@ feature 'API Users Page', type: :feature do
       expect(page.current_path).to eq new_user_session_path
     end
   end
-  # scenario 'can be access from the home page navigation' do
-    # before do
-    #   visit new_user_session_path
-    #    fill_in 'user[login]', with: user.username
-    #     fill_in 'user[password]', with: user.password
-    #     click_button 'Sign in'
-    # end
-  #   visit root_path
-  #   click_link('test-api-users')
-  #   expect(page.current_path).to eq environment_admin_users_path
-  # end
+
+  context 'user signed in' do
+    let(:user) { create(:user, :admin) }
+
+    before do
+      allow_any_instance_of(Admin::User).to receive(:all).and_return([admins])
+      visit new_user_session_path
+      within(:css, 'form.user-signin-form') do
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+      end
+      click_button 'Sign in'
+    end
+
+    scenario 'can be access from the home page navigation' do
+      visit root_path
+      click_link('staging-api-users')
+
+      expect(page.current_path).to eq environment_admin_users_path(environment: 'staging')
+    end
+
+    scenario 'can see a list of user details' do
+      visit environment_admin_users_path(environment: 'staging')
+      expect(page.has_content?(admins['username'])).to be true
+      expect(page.has_content?(admins['name'])).to be true
+      expect(page.has_content?(admins['authentication_token'])).to be true
+      expect(page.has_content?(admins['email'])).to be true
+      expect(page.has_content?(admins['role'])).to be true
+      expect(page.has_content?(admins['daily_requests'])).to be true
+      expect(page.has_content?(admins['monthly_requests'])).to be true
+    end
+  end
 end
