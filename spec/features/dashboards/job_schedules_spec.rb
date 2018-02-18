0639daf6ba5488879c60c33b3468672640ad5865
Oliver Stigley <oliver@boost.co.nz>
2018-02-19 10:37:07 +1300

    harvest job spec for one off schedules: clicking edit, delete and pause

--- spec/features/dashboards/job_schedules_spec.rb
+++ spec/features/dashboards/job_schedules_spec.rb
@@ -12,13 +12,12 @@ RSpec.feature 'Job Schedules', type: :feature do
     end
   end
 
-
-  context 'one off schedules' do
+  context 'One off schedules' do
     before do
       allow_any_instance_of(Partner).to receive(:update_apis)
       allow_any_instance_of(Source).to receive(:update_apis)
       allow(LinkCheckRule).to receive(:create)
-      expect(HarvestSchedule).to receive(:all).and_return([one_off_schedule])
+      allow(HarvestSchedule).to receive(:all).and_return([one_off_schedule])
 
       visit new_user_session_path
       within(:css, 'form.user-signin-form') do
@@ -33,17 +32,47 @@ RSpec.feature 'Job Schedules', type: :feature do
     let(:parser) { create(:parser, source: source) }
     let(:one_off_schedule) { build(:harvest_schedule, recurrent: false, parser_id: parser.id) }
 
-    scenario 'displayes one off schedule details' do
+    before :each do
       one_off_schedule.id = 1
       visit environment_harvest_schedules_path(environment: 'staging')
+    end
 
-       expect(page.has_content?(one_off_schedule.parser.name)).to be true
-       expect(page.has_content?(I18n.l(one_off_schedule.start_time, format: :long))).to be true
-       expect(page.has_content?(one_off_schedule.environment)).to be true
-       expect(page.has_content?(one_off_schedule.mode)).to be true
-       # expect(page.has_content?(one_off_schedule)).to be true
-       # expect(page.has_content?(one_off_schedule)).to be true
-       # expect(page.has_content?(one_off_schedule)).to be true
+    scenario 'displays one off schedule details' do
+      expect(page.has_content?(one_off_schedule.parser.name)).to be true
+      expect(page.has_content?(I18n.l(one_off_schedule.start_time, format: :long))).to be true
+      expect(page.has_content?(one_off_schedule.environment)).to be true
+      expect(page.has_content?(one_off_schedule.mode)).to be true
+    end
+
+    scenario 'can click Edit link to go to update page', js: true do
+      expect(HarvestSchedule).to receive(:find).and_return(one_off_schedule)
+      click_link('Edit')
+
+      expect(page.current_path).to eq edit_environment_harvest_schedule_path(environment: 'staging', id: 1)
+    end
+
+    scenario 'can click Delete link to delete job', js: true do
+      expect(HarvestSchedule).to receive(:find).and_return(one_off_schedule)
+      expect(one_off_schedule).to receive(:destroy)
+
+      click_link('Delete')
+    end
+
+    scenario 'can click Resume link to pause job', js: true do
+      expect(HarvestSchedule).to receive(:find).and_return(one_off_schedule)
+      expect(one_off_schedule).to receive(:update_attributes).with('status'=>'active')
+
+      find('input[value="Resume"]').click
+    end
+
+    scenario 'can click Pause link to pause job', js: true do
+      one_off_schedule.status = 'active'
+      visit environment_harvest_schedules_path(environment: 'staging')
+
+      expect(HarvestSchedule).to receive(:find).and_return(one_off_schedule)
+      expect(one_off_schedule).to receive(:update_attributes).with('status'=>'paused')
+
+      find('input[value="Pause"]').click
     end
   end
 end
