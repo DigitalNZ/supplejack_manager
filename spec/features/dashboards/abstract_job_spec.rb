f431a4c75f80e90479398f278c8f842b6e09cd7d
Oliver Stigley <oliver@boost.co.nz>
2018-02-19 17:18:34 +1300

    active jobs dashboard spec displays duration for finished jobs

--- spec/features/dashboards/abstract_job_spec.rb
+++ spec/features/dashboards/abstract_job_spec.rb
@@ -44,7 +44,7 @@ RSpec.feature 'Abscrtract Job Dashboard', type: :feature do
       visit environment_abstract_jobs_path(status: 'active', environment: 'staging')
     end
 
-    scenario 'displays job details' do
+    scenario 'displays active job details' do
       expect(page.has_content?(abstract_job.parser.name))
       expect(page.has_content?(abstract_job.mode))
       expect(page.has_content?(abstract_job.user.name))
@@ -55,6 +55,44 @@ RSpec.feature 'Abscrtract Job Dashboard', type: :feature do
     scenario 'has links to the parser and the harvest job details' do
       expect(page.has_link?(abstract_job.parser.name, parser_parser_version_path(parser_id: parser.id, id: abstract_job.version_id))).to be true
       expect(page.has_link?('View details', environment_harvest_job_path('staging', id: abstract_job.id))).to be true
+    end
+  end
+
+  context 'Finished jobs', js: true do
+    before do
+      allow_any_instance_of(Partner).to receive(:update_apis)
+      allow_any_instance_of(Source).to receive(:update_apis)
+      allow(LinkCheckRule).to receive(:create)
+      visit new_user_session_path
+      within(:css, 'form.user-signin-form') do
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+      end
+
+      click_button 'Sign in'
+
+      allow(AbstractJob).to receive(:search).and_return Kaminari::PaginatableArray.new([abstract_job], {limit: 50, offset: 0, total_count: 1})
+    end
+
+    let(:source) { create(:source) }
+    let(:parser) { create(:parser, source: source) }
+    let(:abstract_job) do
+      build(:abstract_job, status: 'finished',
+                           mode: 'Normal',
+                           user_id: user.id,
+                           parser_id: parser.id,
+                           start_time: Time.zone.now - 1.hour,
+                           _type: 'HarvestJob',
+                           version_id: '999',
+                           duration: 1111.11)
+    end
+
+    before :each do
+      visit environment_abstract_jobs_path(status: 'finished', environment: 'staging')
+    end
+
+    scenario 'displays finished job duration' do
+      expect(page.has_content?(ChronicDuration.output(abstract_job.duration.to_i)))
     end
   end
 end
