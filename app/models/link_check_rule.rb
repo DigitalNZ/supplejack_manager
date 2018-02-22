504cae1f9ec8ea531b09c86fdd0dc9ef897d4f95
Theo Paul <theo.markkus.paul@gmail.com>
2018-02-22 14:22:16 +1300

    Remove Supplejack License text from ruby files

--- app/models/link_check_rule.rb
+++ app/models/link_check_rule.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-# The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are
-# third party components that are licensed under the MIT license or otherwise publicly available.
-# See https://github.com/DigitalNZ/supplejack_manager for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
-# http://digitalnz.org/supplejack
-
 # app/models/link_check_rule.rb
 class LinkCheckRule < ActiveResource::Base
   include EnvironmentHelpers
--- spec/factories/snippet.rb
+++ spec/factories/snippet.rb
@@ -1,11 +1,3 @@
-# The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are
-# third party components that are licensed under the MIT license or otherwise publicly available.
-# See https://github.com/DigitalNZ/supplejack_manager for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
-# http://digitalnz.org/supplejack
-
 FactoryBot.define do
   factory :snippet do
     name      'Copyright'
