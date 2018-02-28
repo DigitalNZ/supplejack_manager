05eea538ba7df966423c7da5ce85105fd2b8d1b6
Theo Paul <theo.markkus.paul@gmail.com>
2018-03-01 10:05:42 +1300

    Fix failing ability_spec

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-require 'spec_helper'
-=======
-# The majority of The Supplejack Manager code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are
-# third party components that are licensed under the MIT license or otherwise publicly available.
-# See https://github.com/DigitalNZ/supplejack_manager for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
-# http://digitalnz.org/supplejack
-
 require 'rails_helper'
->>>>>>> master
 require 'cancan/matchers'
 
 describe Ability do
