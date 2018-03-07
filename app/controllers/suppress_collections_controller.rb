bcf126ece643011e83a35039070bd48892ece793
Gustavo Motizuki <gustavo@boost.co.nz>
2018-03-08 11:35:02 +1300

    Display suppress information for each collection and display the last 10 with status modified

--- app/controllers/suppress_collections_controller.rb
+++ app/controllers/suppress_collections_controller.rb
@@ -5,13 +5,15 @@ class SuppressCollectionsController < ApplicationController
   respond_to :html, :json
 
   def index
-    @response = RestClient.get("#{fetch_env_vars['API_HOST']}/harvester/sources", { params: {:"source[status]" => "suppressed", api_key: fetch_env_vars['HARVESTER_API_KEY'] } })
-    @blacklisted_sources = JSON.parse(@response)
+    @blacklisted_response = RestClient.get("#{fetch_env_vars['API_HOST']}/harvester/sources", { params: {:"source[status]" => "suppressed", api_key: fetch_env_vars['HARVESTER_API_KEY'] } })
+    @blacklisted_sources = JSON.parse(@blacklisted_response)
+    @top_10_response = RestClient.get("#{fetch_env_vars['API_HOST']}/harvester/sources", { params: {:"source[status]" => "active", limit: 10, order_by: 'status_updated_at', api_key: fetch_env_vars['HARVESTER_API_KEY'] } })
+    @top_10_sources = JSON.parse(@top_10_response)
   end
 
   def update
     begin
-      RestClient.put("#{fetch_env_vars['API_HOST']}/harvester/sources/#{params[:id]}", { source: { status: params[:status] }, api_key: fetch_env_vars['HARVESTER_API_KEY'] })
+      RestClient.put("#{fetch_env_vars['API_HOST']}/harvester/sources/#{params[:id]}", { source: { status: params[:status], status_updated_by: current_user.name }, api_key: fetch_env_vars['HARVESTER_API_KEY'] })
     rescue RestClient::Exception => e
       Rails.logger.error "Exception #{e} when attempting to post to API"
     end
