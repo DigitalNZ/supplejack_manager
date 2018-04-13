17dfe7bb636192598afa9518a513445a434d81f8
Reinkaos <richard@boost.co.nz>
2018-04-13 13:54:53 +1200

    Update the activities controller spec

--- spec/controllers/admin/activites_controller_spec.rb
+++ spec/controllers/admin/activites_controller_spec.rb
@@ -2,11 +2,40 @@
 
 require 'rails_helper'
 
-RSpec.describe Admin::ActivitesController do
+RSpec.describe Admin::ActivitiesController do
+  let(:user) { create(:user, :admin, email: 'info@boost.co.nz') }
+
+  before do
+    sign_in(user)
+  end
+
   describe '#index' do
-    it 'returns a succesfull status code' do
-      get :index
-      expect(response).to have_http_status(200)
+    context 'html' do
+      before do
+        get :index, params: { environment: 'development' }
+      end
+
+      it 'returns a succesfull status code' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'renders the :index template' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'csv' do
+      before do
+        get :index, params: { environment: 'development' }, format: :csv
+      end
+
+      it 'renders a succesful status code' do
+        expect(response.status).to eq 200
+      end
+
+      it 'renders the CSV file' do
+        expect(response.header['Content-Type']).to include 'text/csv'
+      end
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 describe UsersController do
