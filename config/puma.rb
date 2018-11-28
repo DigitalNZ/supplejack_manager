fcf4916c29e2623f550188c9b7b3319421607bd3
Gustavo Motizuki <gustavo@boost.co.nz>
2018-11-28 14:43:26 +1300

    Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,3 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RAILS_ENV'] || 'development'
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
