826daec705a494da0f1812214d2c491ac2d2867a
Reinkaos <richard@boost.co.nz>
2019-01-30 13:41:53 +1300

    Remove methods that have been given to the worker

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # Handle authentication with the worker here
   end
 end
--- app/models/preview.rb
+++ app/models/preview.rb
@@ -19,7 +19,7 @@ class Preview < ActiveResource::Base
   end
 
   def id
-    self._id
+    _id
   end
 
   def harvest_failure?
@@ -42,36 +42,12 @@ class Preview < ActiveResource::Base
     JSON.pretty_generate(JSON.parse(self.api_record)) unless self.api_record.nil?
   end
 
-  def api_record_output
-    CodeRay.scan(api_record_json, :json).html(:line_numbers => :table).html_safe
-  end
-
-  def harvested_attributes_json
-    JSON.pretty_generate(JSON.parse(self.harvested_attributes))
-  end
-
   def harvested_attributes_output
     CodeRay.scan(harvested_attributes_json, :json).html(:line_numbers => :table).html_safe
   end
 
-  def field_errors?
-    JSON.parse(self.field_errors).any? unless self.field_errors.nil?
-  end
-
-  def field_errors_output
-    CodeRay.scan(field_errors_json, :json).html(:line_numbers => :table).html_safe if field_errors?
-  end
-
   def validation_errors_output
     JSON.parse(self.validation_errors) unless self.validation_errors.nil?
-  end
-
-  def harvest_failure_output
-    JSON.parse(self.harvest_failure) if self.harvest_failure.present?
-  end
-
-  def harvest_job_errors_output
-    JSON.parse(self.harvest_job_errors) if self.harvest_job_errors.present?
   end
 
   def validation_errors?
@@ -82,24 +58,7 @@ class Preview < ActiveResource::Base
     self.deletable
   end
 
-  def raw_output
-    CodeRay.scan(self.send("pretty_#{self.format}_output"), self.format.to_sym).html(line_numbers: :table).html_safe
-  end
-
-  def pretty_xml_output
-    self.raw_data
-  end
-
-  def pretty_json_output
-    JSON.pretty_generate(JSON.parse(self.raw_data))
-  end
-
-  def field_errors_json
-    JSON.pretty_generate(JSON.parse(self.field_errors)) if field_errors?
-  end
-
   def errors?
     harvest_failure? or validation_errors? or field_errors? or harvest_job_errors.present?
   end
-
 end
