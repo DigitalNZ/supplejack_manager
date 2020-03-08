077eddc94c4c51a5f3fd43a9798d98dc8af11f27
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-03-09 11:08:45 +1300

    simplify the spec configuration to make the upgrade easier

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'database_cleaner/mongoid'
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
---
+++ spec/support/devise.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'devise'
+
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+
+  config.after :each do
+    Warden.test_reset!
+  end
+end
