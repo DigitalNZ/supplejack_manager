419308c76e9b197ae39ba7b85aac615d3ee0d648
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-03-11 17:14:16 +1300

    refactor the rename form interaction by using toggler from foundation...
    The style has been changed as well to stick the rename button with the input

--- app/assets/javascripts/parser_templates.js
+++ app/assets/javascripts/parser_templates.js
@@ -1,9 +1,4 @@
 $(function() {
-  $('#rename-parser-template-action').click(function() {
-    $('#parser-template-title').hide();
-    return $('#hidden-parser-template-form').show();
-  });
-
   $('#cancel-parser-template-delete').click(function() {
     return $('#delete-parser-template-alert .close-reveal-modal').trigger('click');
   });
--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -1,9 +1,4 @@
 $(function() {
-  $('#rename-snippet-action').click(function() {
-    $('#snippet-title').hide();
-    return $('#hidden-snippet-form').show();
-  });
-
   $('#cancel-snippet-delete').click(function() {
     return $('#delete-snippet-alert .close-reveal-modal').trigger('click');
   });
