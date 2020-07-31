49212dc9ab2f373016b000d4304f533578580fd4
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-07-31 14:25:31 +1200

    fix: add respond_to to the harvest_jobs_controller
    CloudFront is changing the request and the controller was throwing an error UnknownFormat because the recognized format was www_url_encoded instead of JS

--- app/controllers/harvest_jobs_controller.rb
+++ app/controllers/harvest_jobs_controller.rb
@@ -2,6 +2,7 @@
 
 # app/controllers/harvest_jobs_controller.rb
 class HarvestJobsController < ApplicationController
+  respond_to :js, :html
   skip_before_action :verify_authenticity_token
 
   before_action :set_worker_environment
@@ -13,6 +14,8 @@ class HarvestJobsController < ApplicationController
   def create
     @harvest_job = HarvestJob.new(harvest_job_params)
     @harvest_job.save
+
+    respond_to :js
   end
 
   def update
@@ -20,11 +23,11 @@ class HarvestJobsController < ApplicationController
     @harvest_job.update_attributes(harvest_job_params)
   end
 
-  def set_worker_environment
-    set_worker_environment_for(HarvestJob)
-  end
-
   private
+    def set_worker_environment
+      set_worker_environment_for(HarvestJob)
+    end
+
     def harvest_job_params
       params.require(:harvest_job).permit(:parser_id, :version_id, :user_id, :environment, :mode, :limit, :status, enrichments: [])
     end
