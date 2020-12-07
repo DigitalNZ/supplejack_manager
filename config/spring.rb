864e102bd671b50ef1d9e1cb694efcc09e01bc9f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-12-07 18:43:18 +0100

    conf: `bundle exec rails app:update`

--- config/spring.rb
+++ config/spring.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-%w[
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-  config/application.yml
-].each { |path| Spring.watch(path) }
+Spring.watch(
+  '.ruby-version',
+  '.rbenv-vars',
+  'tmp/restart.txt',
+  'tmp/caching-dev.txt',
+  'config/application.yml'
+)
