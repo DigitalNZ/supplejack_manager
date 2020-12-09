0da3ea6beebd0b7fb734e85e114c44eac7ed0963
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-12-09 15:38:23 +0100

    fix: deprecation warning

--- lib/custom_logger/logger.rb
+++ lib/custom_logger/logger.rb
@@ -1,36 +1,27 @@
 # frozen_string_literal: true
 
-unless Rails.env.test? || Rails.env.development?
-  module ActiveSupport
-    module TaggedLogging
-      module Formatter
-        def call(severity, time, progname, data)
-          data = { msg: data.to_s } unless data.is_a?(Hash)
-          tags = current_tags
-          data[:tags] = tags if tags.present?
-          _call(severity, time, progname, data)
-        end
+return if Rails.env.test? || Rails.env.development?
+
+module ActiveSupport
+  module TaggedLogging
+    module Formatter
+      def call(severity, time, progname, data)
+        data = { msg: data.to_s } unless data.is_a?(Hash)
+        tags = current_tags
+        data[:tags] = tags if tags.present?
+        super(severity, time, progname, data)
       end
     end
   end
+end
 
-  module CustomLogger
-    class Logger < Ougai::Logger
-      include ActiveSupport::LoggerThreadSafeLevel
-      include LoggerSilence
+module CustomLogger
+  class Logger < Ougai::Logger
+    include ActiveSupport::LoggerThreadSafeLevel
+    include ActiveSupport::LoggerSilence
 
-      def initialize(*args)
-        super
-        after_initialize if respond_to? :after_initialize
-      end
-
-      def create_formatter
-        if Rails.env.development? || Rails.env.test?
-          Ougai::Formatters::Readable.new
-        else
-          Ougai::Formatters::Bunyan.new
-        end
-      end
+    def create_formatter
+      Ougai::Formatters::Bunyan.new
     end
   end
 end
