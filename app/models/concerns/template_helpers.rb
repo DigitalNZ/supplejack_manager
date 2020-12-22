3205b63214f8b796b50b3c36a7baa355a2dd531d
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-12-22 15:39:46 +0100

    chore: fix the readibility of the template_parsers

--- app/models/concerns/template_helpers.rb
+++ app/models/concerns/template_helpers.rb
@@ -11,20 +11,25 @@ module TemplateHelpers
   end
 
   def apply_parser_template!
-    if self.content.nil?
+    return if self.content.nil?
 
-      parser_template = []
-      parser_template << "class #{self.name.gsub(/\s/, "_").camelize} < SupplejackCommon::#{self.strategy.capitalize}::Base"
+    parser_template = []
+    parser_template << "class #{parser_classname} < SupplejackCommon::#{self.strategy.capitalize}::Base"
 
-      if parser_template_name.present?
-        template = ParserTemplate.find_by_name(parser_template_name)
-        parser_template << "\t" + template.content.gsub(/\n/, "\n\t")
-      end
-
-      parser_template << 'end'
-
-      self.content = parser_template.join("\n\n")
-      self.message = 'Applied parser template.'
+    if parser_template_name.present?
+      template = ParserTemplate.find_by_name(parser_template_name)
+      parser_template << "\t" + template.content.gsub(/\n/, "\n\t")
     end
+
+    parser_template << 'end'
+
+    self.content = parser_template.join("\n\n")
+    self.message = 'Applied parser template.'
   end
+
+  protected
+
+    def parser_classname
+      self.name.gsub(/\s/, "_").camelize
+    end
 end
