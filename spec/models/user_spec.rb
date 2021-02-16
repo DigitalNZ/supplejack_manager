1a2bad08d846bae5b2e07159511af204e98f5bbf
Richard Matthews <richard@boost.co.nz>
2021-02-17 10:41:00 +1300

    fix for user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,6 +73,7 @@ RSpec.describe User do
 
   describe 'generate_totp' do
     it 'generate the totp after the user has been created' do
+      MFA_ENABLED = true
       user = User.new(name: 'test', password: 'password', password_confirmation: 'password', email: 'test@test.co.nz')
       expect(user.otp_secret_key).to eq nil
       user.save!
