dbd25ea5d4d0dce53fccba6d529c69d743013d8d
Richard Matthews <richard@boost.co.nz>
2021-02-17 10:19:24 +1300

    Update the example and fix loading of MFA enabled

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ Bundler.require(*Rails.groups)
 APPLICATION_ENVS = YAML.load_file('config/application.yml').keys - ['development', 'test'] rescue []
 APPLICATION_ENVIRONMENT_VARIABLES = YAML.load(ERB.new(File.read('config/application.yml')).result)
 
+MFA_ENABLED = ActiveModel::Type::Boolean.new.cast(APPLICATION_ENVIRONMENT_VARIABLES[Rails.env]['MFA_ENABLED'])
+
 begin
   ENV.update YAML.load(ERB.new(File.read('config/application.yml')).result)[Rails.env]
 rescue StandardError
