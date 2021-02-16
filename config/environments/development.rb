81e17e59ff1455208d21f43277f6c54d133b4be7
Richard Matthews <richard@boost.co.nz>
2021-02-17 09:48:13 +1300

    remove log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,4 @@ Rails.application.configure do
   # ALL custom configs comes  under this line
   DEFAULT_URL_OPTIONS = { :host => 'localhost:3000' }
   config.action_mailer.default_url_options = DEFAULT_URL_OPTIONS
-
-  Mongoid.logger = Logger.new($stdout)
-  Mongo::Logger.logger = Logger.new($stdout)
 end
