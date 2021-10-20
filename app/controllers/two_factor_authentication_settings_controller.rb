f89bef5690f077cbfcd5da1d05336d36219e2206
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2021-10-20 17:19:23 +0200

    fix: fixed the edit of 2FA on other users

--- app/controllers/two_factor_authentication_settings_controller.rb
+++ app/controllers/two_factor_authentication_settings_controller.rb
@@ -4,16 +4,23 @@ class TwoFactorAuthenticationSettingsController < ApplicationController
   def create
     return unless MFA_ENABLED
 
-    unless current_user.authenticate_totp(params.dig(:two_factor_authentication_settings, :otp))
+    user = User.find(mfa_params[:user_id])
+
+    unless user.authenticate_totp(mfa_params[:otp])
       redirect_to(
-        edit_user_path(current_user),
+        edit_user_path(user),
         alert: 'Incorrect two factor authentication code'
       ) && return
     end
 
     redirect_to(
-      edit_user_path(current_user),
+      edit_user_path(user),
       notice: 'Successfully enabled two factor authentication'
     )
   end
+
+  private
+    def mfa_params
+      params.require(:two_factor_authentication_settings).permit(:otp, :user_id)
+    end
 end
