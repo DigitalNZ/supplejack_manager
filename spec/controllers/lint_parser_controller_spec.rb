1d5f05116fbb70c32ea03d284aacf6193dde1133
Edwin Rozario <rozarioed@gmail.com>
2022-01-11 14:59:39 +1300

    test(LintParserController)

---
+++ spec/controllers/lint_parser_controller_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe LintParserController do
+  before do
+    allow_any_instance_of(Partner).to receive(:update_apis)
+    allow_any_instance_of(Source).to receive(:update_apis)
+    allow(LinkCheckRule).to receive(:create)
+  end
+
+  let(:source)  { create(:source) }
+  let(:parser)  { create(:parser, source_id: source) }
+  let(:user)    { create(:user, :admin) }
+
+  before { sign_in user }
+
+  describe 'GET show' do
+    before { get :show, params: { id: parser.id } }
+
+    it 'assigns parser' do
+      expect(assigns(:parser)).to eq parser
+    end
+
+    it 'assigns rubocop warnings for the parser script' do
+      expect(assigns(:warnings)).to include 'Style/FrozenStringLiteralComment: Missing frozen string literal comment'
+    end
+  end
+end
