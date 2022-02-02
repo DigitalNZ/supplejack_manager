6b4d2685fe8ccea5515435b151bbd872afc33919
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-02-02 15:46:37 +0100

    feat: force the user to be logged in to access web sockets

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,23 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      # This is a websocket so we have no warden and no session here
+      # How to reuse the login made with devise?
+      # http://www.rubytutorial.io/actioncable-devise-authentication/
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if verified_user = env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
