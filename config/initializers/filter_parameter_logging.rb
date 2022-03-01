da59c1e96f15205dbd4c5ce82cc698bd6bfccb82
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-03-01 23:18:29 +0100

    conf(rails-7): ran "bin/rails app:update" and resolved conflicts

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,17 +1,18 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[
-  password
-  password_confirmation
-  otp_attempt
-  parser_code
-  raw_data
-  harvested_attributes
-  harvest_failure
-  enrichment_failures
-  api_record
-  content
+# Configure parameters to be filtered from the log file. Use this to limit dissemination of
+# sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
+# notations and behaviors.
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+  :password,
+  :password_confirmation,
+  :otp_attempt,
+  :parser_code,
+  :raw_data,
+  :harvested_attributes,
+  :harvest_failure,
+  :enrichment_failures,
+  :api_record,
+  :content,
 ]
