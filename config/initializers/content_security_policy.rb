d54a2a05f65c3d92b6867663d4c035e09a20d880
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-07-30 17:33:45 +0200

    conf: upgraded rails to 7.1

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,36 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+# Define an application-wide content security policy.
+# See the Securing Rails Applications Guide for more information:
+# https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src     :none
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src     :none
 
-  # alphabetically ordered
-  policy.base_uri        :none
-  policy.connect_src     :self
-  policy.img_src         :self, :data, 'https://ajax.googleapis.com'
-  policy.font_src        :self
-  policy.form_action     :self
-  policy.frame_ancestors :none
-  policy.object_src      :none
-  policy.script_src      :self
-  policy.style_src       :self, 'https://ajax.googleapis.com'
+    # alphabetically ordered
+    policy.base_uri        :none
+    policy.connect_src     :self
+    policy.img_src         :self, :data, 'https://ajax.googleapis.com'
+    policy.font_src        :self
+    policy.form_action     :self
+    policy.frame_ancestors :none
+    policy.object_src      :none
+    policy.script_src      :self
+    policy.style_src       :self, 'https://ajax.googleapis.com'
 
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
 
-  policy.upgrade_insecure_requests Rails.env.in? %w[uat staging production]
+    policy.upgrade_insecure_requests Rails.env.in? %w[uat staging production]
+  end
 end
-
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
