1203d658bc5a2d4d8f5ba5223a15051f1db432b2
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-10-23 16:29:44 +0200

    conf: disabled the APM in JS

--- app/javascript/src/apm.js
+++ app/javascript/src/apm.js
@@ -1,16 +1,17 @@
 import { init as initApm } from "@elastic/apm-rum";
 
-if (process.env.NODE_ENV == "production") {
-  initApm({
-    // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-    serviceName: "Supplejack Manager",
+initApm({
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: "Supplejack Manager",
 
-    // Set custom APM Server URL (default: http://localhost:8200)
-    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: process.env.ELASTIC_APM_SERVER_URL,
 
-    // Set service version (required for sourcemap feature)
-    serviceVersion: "",
+  // Set service version (required for sourcemap feature)
+  serviceVersion: "",
 
-    environment: process.env.NODE_ENV,
-  });
-}
+  environment: process.env.NODE_ENV,
+  active: false,
+  // to enable the elastic apm again
+  // active: process.env.RAILS_ENV == "production",
+});
